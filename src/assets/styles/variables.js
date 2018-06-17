15f8cec895f3e7fe09bd12c9bf2992659ca2cca0
denisden83 <denisden83@gmail.com>
2018-06-17 16:49:50 +0200

    finish css page index

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,8 +1,9 @@
 module.exports = {
   white: "#efeee9",
   beige: "#eeebdf",
-  green: "#16c2aa",
   "text-color": "#455a64",
   grey: "#373e42",
-  "light-grey": "#dfdcd5"
+  "light-grey": "#dfdcd5",
+  green: "#639055",
+  "dark-green": "#498038"
 };
